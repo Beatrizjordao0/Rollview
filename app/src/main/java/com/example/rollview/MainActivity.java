e4ba89fce77ae5124b360dda88dbae807a8c71ac
Alyson Coutinho <coutinhoalyson138@gmail.com>
2026-05-09 01:37:52 -0300

    Foi iniciado uma nova tela, parte visual parcialmente feita. falta alguns detalhes e configurar também o back end. Também foi feito funções dos botões da tela inicial, agora ele direciona para as telas indicadas. também ajustei o nome de português para inglês(entrar e cadastro) da tela inicial.

--- app/src/main/java/com/example/rollview/MainActivity.java
+++ app/src/main/java/com/example/rollview/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.rollview;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,17 +13,41 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button buttonLogin;
+    Button buttonSignup;
+    TextView skipButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
+
+        // tela inicial
         setContentView(R.layout.activity_main);
+
+        skipButton = findViewById(R.id.skip);
+        buttonLogin = findViewById(R.id.login);
+        buttonSignup = findViewById(R.id.signup);
+
+        skipButton.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(intent);
+        });
+
+        buttonLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+        });
+
+        buttonSignup.setOnClickListener(v -> {
+            Intent intent = new Intent(MainActivity.this, SignupActivity.class);
+            startActivity(intent);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
-
 }
